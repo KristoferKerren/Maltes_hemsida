0687f3976b722124fca6f8113a762e135d57f1e4
Kristofer Kerrén <kristofer.kerren@gmail.com>
2024-12-27 11:26:28 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("knapp");
+
+  button.addEventListener("click", () => {
+    alert(
+      "HA! Du klickade fast du inte fick, nu får du ge mig extra många och fina julklappar! 🤣🤣🤣🤣"
+    );
+  });
+});
